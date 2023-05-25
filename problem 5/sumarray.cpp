59c17a6db3caf0713c8070955f59db64f34f6613
ChikhaliyaYash <chikhaliyayash899@gmail.com>
2023-05-26 00:06:16 +0530

    solved array problems

---
+++ problem 5/sumarray.cpp
@@ -0,0 +1,24 @@
+//sum of all elements in array 
+#include<iostream>
+using namespace std;
+
+void sumarray(int sum[] , int n){
+
+    int finalsum = 0;
+    for(int i = 0 ; i <= n ; i++){
+
+        finalsum = sum[i] ;
+        cout << "Array elements" << endl ; 
+
+    }
+    return 
+
+}
+int main(){
+
+    int sum[10];
+    int n = 10;
+
+    cout 
+
+}
