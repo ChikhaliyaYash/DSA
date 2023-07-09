51c4c5149232c8eb43cda27f7dc113440602fbc6
ChikhaliyaYash <chikhaliyayash899@gmail.com>
2023-07-09 18:01:04 +0530

    string

--- problem 8/stringlength.cpp
+++ problem 8/stringlength.cpp
@@ -1,1 +1,23 @@
+//how to find string length [first solution]
+
 #include<iostream>
+using namespace std;
+int main() {
+    int i, count = 0;
+    string str = "Scaler";
+    for (i = 0; str[i]; i++) {
+        count++;
+    }
+    cout << "The length of the string is " << count << endl;
+}    
+
+//second solution of find string length using function [.size[] , .length[]] 
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char string[] = "I love C++";
+    cout << "Length of the given string is "<< strlen(string) << endl;
+}
